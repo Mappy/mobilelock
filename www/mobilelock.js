9355567d6716e8cb2d050a475566a108b7fc1f08
Grégory Paul <paulgreg@gmail.com>
2013-11-28 15:50:47 +0100

    List devices

--- www/mobilelock.js
+++ www/mobilelock.js
@@ -1,10 +1,14 @@
 var MobileLock = (function(undefined) {
 
+    var $devices = $('#devices ul');
+    var tplDevices = _.template($('#tpl-devices').text());
 
-    //var $tplTarget = $('#tpl-target');
-    //var tplTarget = _.template($tplTarget.text());
+    var devices = [ 
+        { "ua": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0"}
+    ];
 
-    return {
-        'launch': function() {} 
-    };
+    var launch = function() {
+        $devices.html(tplDevices({'devices':devices}));
+    }
+    $(launch);
 })();
